1c535c4a1e4b0b486ff95d396ced3f34334f0eaf
Aditya-gautam21 <adigaur121@gmail.com>
2026-05-11 21:32:02 +0530

    feat: creating vectorstore from embeddings

---
+++ services/vectorstore.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+from langchain_community.vectorstores import FAISS
+from services.embeddings import get_embeddings
+
+chunks, embeddings = get_embeddings()
+INDEX_PATH = Path('/home/adityagautam/Desktop/Projects/Teleport-RAG-assignment/data')
+
+def create_vecotrstore():
+    vector_store = FAISS.from_documents(chunks, embeddings)
+
+    vector_store.save_local(INDEX_PATH)
+    return vector_store
+
+def load_vectorstore():
+    if not os.path.exists(INDEX_PATH):
+        vector_store = create_vecotrstore()
+
+    else:
+        vector_store = FAISS.load_local(
+            folder_path=INDEX_PATH,
+            embeddings=embeddings,
+            allow_dangerous_deserialization=True
+            )
+
+    return vector_store
