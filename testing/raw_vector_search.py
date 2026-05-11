cc77cceae87c092597ef7eda24af1ce5b8291188
Aditya-gautam21 <adigaur121@gmail.com>
2026-05-11 23:09:05 +0530

    minor changes

--- testing/raw_vector_search.py
+++ testing/raw_vector_search.py
@@ -13,5 +13,5 @@ def raw_search(query: str, k: int = 3):
     return results
 
 if __name__ == '__main__':
-    benchmark_query = "How does the system handle peak load?"
+    benchmark_query = "How can external knowledge bases prevent language models from making up false information?"
     raw_search(benchmark_query)
