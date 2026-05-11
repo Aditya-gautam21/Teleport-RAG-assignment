789bfeede23f895d22f456d86adca8c150a50f17
Aditya-gautam21 <adigaur121@gmail.com>
2026-05-11 23:24:45 +0530

    feat: separated chunks and embedding logic

--- services/embeddings.py
+++ services/embeddings.py
@@ -1,9 +1,5 @@
 import faiss
 from langchain_huggingface import HuggingFaceEmbeddings
-from services.chunker import text_splitter
 
 def get_embeddings():
-    chunks = text_splitter()
-    embeddings = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
-
-    return chunks, embeddings
+    return HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
--- services/vectorstore.py
+++ services/vectorstore.py
@@ -2,21 +2,25 @@ import os
 from pathlib import Path
 from langchain_community.vectorstores import FAISS
 from services.embeddings import get_embeddings
-
-chunks, embeddings = get_embeddings()
+from services.chunker import text_splitter
 
 INDEX_PATH = Path('/home/adityagautam/Desktop/Projects/Teleport-RAG-assignment/data/vectorstore')
 INDEX_FILE = INDEX_PATH / "index.faiss"
 
-def create_vecotrstore():
+def create_vectorstore():
+    chunks = text_splitter()
+    embeddings = get_embeddings()
+    
     vector_store = FAISS.from_documents(chunks, embeddings)
 
     vector_store.save_local(INDEX_PATH)
     return vector_store
 
 def load_vectorstore():
+    embeddings = get_embeddings()
+    
     if not INDEX_FILE.exists():
-        vector_store = create_vecotrstore()
+        vector_store = create_vectorstore()
 
     else:
         vector_store = FAISS.load_local(
